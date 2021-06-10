a420e96064c64624635d3f98981b3a3f24fa7736
Anatoly Shcherbina <avshcherbina@gmail.com>
2021-06-10 21:25:05 +0300

    Correct code of the example

--- countdown.py
+++ countdown.py
@@ -15,13 +15,21 @@ def get_number() -> int:
     """Input and check a number from 1 to 100."""
     try:
         num = int(input('Enter a positive integer from 1 to 100: '))
-        assert 1 <= num <= 100
         
-        return num       
-    except (ValueError, AssertionError):
-        print('Wrong value, try again.')
-        
-        return 0
+        # if num is not in range 1-100, it will be reassignment = 0
+        if num <= 0:
+            print('Wrong value (your number <= 0), try again.')
+            num = 0
+        elif num > 100:
+            print('Wrong value (your number > 100), try again.')
+            num = 0   
+    except ValueError:
+        # if exeption, num will be assignment = 0
+        print('Wrong value (not a number), try again.')
+        num = 0
+    finally:
+        # return num (num = 0 if not in range or exception)
+        return num
         
 
 def countdown(n: int) -> None:
@@ -35,4 +43,3 @@ def countdown(n: int) -> None:
         
 if __name__ == '__main__':
     main()
-    
