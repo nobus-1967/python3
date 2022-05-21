abeef9eae611fae757bba022d5cbe18223baac1d
nobus-1967 <avshcherbina@gmail.com>
2022-05-21 13:09:41 +0300

    Add a new example (test FizzBuzz using generator)

---
+++ fizzbuzzgeneratortest.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""Test FizzBuzz using generator."""
+
+fizzbuzz = (("Fizz" * (i % 3 == 0) + "Buzz" * (i % 5 == 0) or i)
+            for i in range(1, 10001))
+
+def main():
+    number = next(fizzbuzz)
+    i = 1
+    ok = 0
+    errors = 0
+        
+    print('Starting test:')
+    print('--------------')
+        
+    while number:
+        try:
+            if type(number) == int:
+                print(f'{number}: OK')
+                ok += 1
+            elif type(number) == str:
+                if i % 15 == 0:
+                    assert number == 'FizzBuzz', f'{number} (for {i}): Error'
+                    print(f'{number} (for {i}): OK')
+                    ok += 1
+                elif i % 3 == 0:
+                    assert number == 'Fizz', f'{number} (for {i}): Error'
+                    print(f'{number} (for {i}): OK')
+                    ok +=1
+                elif i % 5 == 0:
+                    assert number == 'Buzz', f'{number} (for {i}: Error'
+                    print(f'{number} (for {i}): OK')
+                    ok +=1
+                else:
+                    assert number == 'FizzBuzz', f'{number} (for {i}): Error'
+                    print(f'{number} (for {i}): OK')
+                    errors += 1
+            else:
+                print(f'{number} (for {i}): Error')
+                errors += 1
+        except AssertionError as e:
+            print(e)
+            errors += 1
+
+        try:
+            number = next(fizzbuzz)
+            i += 1
+        except StopIteration as e:
+            print('--------------')
+            print('Test finished.')
+            print(f'Total:\t{i}')
+            break
+        
+    print(f'Passed:\t{ok}')
+    print(f'Errors:\t{errors}')
+        
+        
+if __name__ == '__main__':
+    main()
