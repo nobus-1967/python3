1f6dcc18add47051f1c1ba4b1a70a33d23bf6cbb
nobus-1967 <avshcherbina@gmail.com>
2022-11-26 21:10:49 +0300

    Correct typos and add some comments.

--- easter.py
+++ easter.py
@@ -3,7 +3,7 @@
 """Date of Orthodox Easter (XXI century)."""
 
 # Константы для работы программы.
-"""Сonstant list."""
+"""Сonstants list."""
 A = 19
 B = 4
 C = 7
@@ -18,29 +18,28 @@ F1 = 4
 F2 = 26
 
 
-
 # Основная функция программы.
 def main():
+    # Запустить основную функцию.
     """Run the main function."""
     # Инициализировать флаг, который будет повторять выполнение программы.
     another = 'д'
-    
+
     # Печатать названия программы.
     print('Определение даты Православной Пасхи в XXI веке')
     print('----------------------------------------------')
-    
+
     # Запустить цикла выполнения программы.
     while another == 'д':
-        
+
         # Получить год для определения Пасхи.
         year = get_year()
         while year is None:
             year = get_year()
-            
+
         # Сообщить пользователю о том, для какого года будет произведён расчёт.
         print(f'Дата Православной Пасхи будет определена для {year} года.')
-        
-                    
+
         # Расчитать дату Пасхи по новому стилю и вывести её.
         easter_date = calculate_easter_date(int(year))
         print(f'Итак, Пасха приходится на {easter_date[0]} {easter_date[1]}.')
@@ -56,26 +55,28 @@ def main():
     print('--------------------------')
     print('Работа программы завершена')
     print('(c) Nobus, 2022')
-    
+
 
 def get_year() -> int | None:
+    # Ввести год для расчета даты Православной Пасхи.
     """Input a year to calculate the date of Orthodox Easter."""
     try:
-        year = int(input('Введите год для определения даты Пасхи: '))  
+        year = int(input('Введите год для определения даты Пасхи: '))
         assert 2000 < year < 2101
-    
+
     except (AssertionError, ValueError):
-                # В случае ошибки повторить ввод года.
-                print('Год введен неверно! Необходимо ввести его заново.')
-                print('(Год - целое число больше 2000 и меньше 2101.)')
-                
-                return None
+        # В случае ошибки вернуть None для повторного ввода года.
+        print('Год введен неверно! Необходимо ввести его заново.')
+        print('(Год - целое число больше 2000 и меньше 2101.)')
+
+        return None
     else:
         # Вернуть год.
         return year
 
 
-def calculate_easter_date(year:int) -> list:
+def calculate_easter_date(year: int) -> list:
+    # Расчитать дату Пасхи.
     """Calculate the Easter's date."""
     a = year % A
     b = year % B
@@ -83,12 +84,12 @@ def calculate_easter_date(year:int) -> list:
     d = ((D2 * a) + D3) % D1
     e = ((E2 * b) + (E3 * c) + (E4 * d) + 6) % E1
     f = d + e
-    
+
     date = [(F1 + f), 'апреля'] if f <= 26 else [(f - 26), 'мая']
-    
+
+    # Вернуть полученную дату (список из числа и месяца).
     return date
 
 
 if __name__ == '__main__':
     main()
-
